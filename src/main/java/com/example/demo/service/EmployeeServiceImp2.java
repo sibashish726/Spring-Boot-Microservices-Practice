2855d0ee129208695a6f4281abcc286d5c099f8b
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2026-01-26 10:02:02 +0530

    POST , GET implementation using jpa

--- src/main/java/com/example/demo/service/EmployeeServiceImp2.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImp2.java
@@ -1,8 +1,9 @@
 package com.example.demo.service;
 
-
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,37 +14,46 @@ import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repo.EmployeeRepo;
 
-
 @Service
 public class EmployeeServiceImp2 implements EmployeeService {
 
-	
 	private static final AtomicLong counter = new AtomicLong(20260001);
-	
+
 	@Autowired
 	private EmployeeRepo employeeRepo;
-	
-	
+
 	@Override
-	public Employee save(Employee employee) {	
-		if(employee.getEmployeeId()==null || employee.getEmail().isEmpty()) {
+	public Employee save(Employee employee) {
+		if (employee.getEmployeeId() == null || employee.getEmail().isEmpty()) {
 			employee.setEmployeeId(String.valueOf(counter.getAndIncrement()));
 		}
-		EmployeeEntity entity= new EmployeeEntity();
+		EmployeeEntity entity = new EmployeeEntity();
 		BeanUtils.copyProperties(employee, entity);
 		employeeRepo.save(entity);
-		
+
 		return employee;
 	}
+
 	@Override
 	public List<Employee> getAllEmployees() {
-		return null;
+		List<EmployeeEntity> employeeEntityList = employeeRepo.findAll();
+		return employeeEntityList.stream().map(employeeEntity -> {
+			Employee employee = new Employee();
+			BeanUtils.copyProperties(employeeEntity, employee);
+			return employee;
+
+		}).collect(Collectors.toList());
+
 	}
-	
+
 	@Override
 	public Employee getEmployeeById(String id) {
-		return  null;
+		EmployeeEntity employeeEntity= employeeRepo.findById(id).get();
+		Employee employee= new Employee();
+		BeanUtils.copyProperties(employeeEntity, employee);
+		return employee;
 	}
+
 	@Override
 	public String deleteEmployeeById(String id) {
 		return null;
